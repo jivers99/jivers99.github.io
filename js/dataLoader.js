a65949dc030aef470a002af60f815bb077c2e17b
Jonathan Ivers <Ivers.Jonathan99@gmail.com>
2026-02-13 13:00:54 -0500

    Changed dataLoader to attempt to fix config screen

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,18 +1,19 @@
-import { buildConfigUI } from './configUI.js';
-import { startGameWithConfig } from './gameState.js';
+import { buildConfigUI } from "./configUI.js";
+import { startGameWithConfig } from "./gameState.js";
 
-fetch('./data/pokemon.json')
+fetch("../data/pokemon.json")
   .then(res => res.json())
   .then(data => {
 
-      window.fullPokemonData = data;
+    console.log("Data loaded"); // ✅ debug line
 
-      buildConfigUI();
+    buildConfigUI();
 
-      document
-        .getElementById("start-game-btn")
-        .addEventListener("click", () => {
-            startGameWithConfig(data);
-        });
+    document
+      .getElementById("start-game-btn")
+      .addEventListener("click", () => {
+        startGameWithConfig(data);
+      });
 
-  });
+  })
+  .catch(err => console.error("LOAD ERROR:", err));
